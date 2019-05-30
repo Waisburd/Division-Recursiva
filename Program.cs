8f1f4871d436b9a4c3e32a5f551aa96f9e83cbdd
Tomas Waisburd <tomaswa@lagash.com>
2019-05-30 16:43:48 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Division
+{
+    class Program
+    {
+
+        static int dividir(int dividendo, int divisor)
+        {
+            int cociente = 1;
+            int nuevoDividendo = dividendo - divisor;
+            if(divisor == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            if(nuevoDividendo < 0)
+            {
+                return 0;
+            } 
+            else if(nuevoDividendo == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                cociente += dividir(nuevoDividendo, divisor);
+                return cociente;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            int dividendo = 3;
+            int divisor = 2;
+            try
+            {
+                Console.WriteLine(dividendo + " / " + divisor + " = " + dividir(dividendo, divisor));
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
